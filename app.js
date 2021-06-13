86fc761792620faa140169c98c40157f776fe04b
rikoniko <78467513+rikoniko@users.noreply.github.com>
2021-06-13 23:43:37 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+//publicフォルダにあるcssを使えるようになる
+
+app.use(express.static('public'));
+
+//フォームから送信された値を受け入れるようになる
+app.use(express.urlencoded({extended: false}));
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'r08020206',
+  database: 'list_app',
+  dateStrings:'date'//日付をdate型にする
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('success');
+});
+
+//トップページの表示
+app.get('/top', (req, res) => {
+  
+  connection.query(
+    'SELECT * FROM items',
+    (error, results) => {
+      console.log(results);
+      //データベースの結果を得る
+      res.render('top.ejs',{items:results});
+    }
+  );
+});
+
+//編集画面の表示
+app.get('/edit/:id',(req,res)=>{
+  connection.query(
+    'select * from items where id=?',
+    [req.params.id],
+    (error,results)=>{
+      res.render('edit.ejs',{items:results[0]});
+    }
+  );
+});
+
+//更新するとき
+app.post('/update/:id',(req,res)=>{
+  connection.query(
+    'update items set message=? where id=?',
+    [req.body.itemMessage,req.params.id],
+    (error,results)=>{
+      res.redirect('/top');
+    }
+  );
+  
+});
+
+//新規のコメントの追加
+app.post('/create',(req,res)=>{
+  var name_db=req.body.itemName
+  var message_db=req.body.itemMessage
+  var day_db=req.body.itemDay
+  connection.query(
+    'INSERT INTO items (name,message,day) VALUES ("' + name_db + '","'+message_db+'","'+day_db+'")',
+    (error, results) => {
+      res.redirect('/top');
+    }
+  );
+
+});
+
+app.get('/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+app.post('/delete/:id',(req,res)=>{
+  connection.query(
+    'delete from items where id=?',
+    [req.params.id],
+    (error,results)=>{
+      res.redirect('/top');
+    }
+  )
+});
+
+app.listen(3000);
